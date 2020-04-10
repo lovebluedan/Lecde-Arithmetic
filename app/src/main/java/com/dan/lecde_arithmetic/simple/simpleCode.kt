b8b31d936f0aa31b310bc61fe93285bb4ffaff86
Mr.dam <326208323@qq.com>
2020-04-10 18:02:44 +0800

    提交代码

--- app/src/main/java/com/dan/lecde_arithmetic/simple/simpleCode.kt
+++ app/src/main/java/com/dan/lecde_arithmetic/simple/simpleCode.kt
@@ -5,7 +5,24 @@ package com.dan.lecde_arithmetic.simple
  * on 2020/4/10 16:17.
  */
 fun main() {
+    var node = Node(1)
+    var oneNode = node
+    var testNode = node
+    for (i in 2..10) {
+        node.next = Node(i)
+        node = node.next!!
+    }
 
+    while (oneNode != null) {
+        print("${oneNode.value} ->")
+        oneNode = oneNode.next ?: break
+    }
+    println("")
+    var re = invert(testNode)
+    while (re != null) {
+        print("${re.value} ->")
+        re = re.next ?: break
+    }
 }
 
 /**
@@ -23,11 +40,19 @@ class Node {
 /**
  * 链表反转
  */
-fun invert(node: Node): Unit {
-    var newNode: Node
-    var curNode = node
+fun invert(node: Node): Node? {
+    var next: Node
+    var preNode: Node? = null
+    var curNode: Node = node
     while (curNode != null) {
-
+        if (curNode.next == null) {
+            preNode!!.value = curNode.value
+            return preNode
+        }
+        next = curNode.next!!
+        curNode.next = preNode
+        preNode = curNode
+        curNode = next
     }
-
+    return preNode
 }
