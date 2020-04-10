be0979b9f62c14642f662eb6291c8748ec1d5658
Mr.dam <326208323@qq.com>
2020-04-10 16:41:08 +0800

    Initial commit

---
+++ app/src/main/java/com/dan/lecde_arithmetic/simple/simpleCode.kt
@@ -0,0 +1,33 @@
+package com.dan.lecde_arithmetic.simple
+
+/**
+ * Create by ldr
+ * on 2020/4/10 16:17.
+ */
+fun main() {
+
+}
+
+/**
+ * 定义链表节点
+ */
+class Node {
+    constructor(data: Int) {
+        value = data
+    }
+
+    var value: Int? = 0
+    var next: Node? = null
+}
+
+/**
+ * 链表反转
+ */
+fun invert(node: Node): Unit {
+    var newNode: Node
+    var curNode = node
+    while (curNode != null) {
+
+    }
+
+}
